4d927473494643487e143ac24a64108c610daaf7
John Shaffer <jshaffer2112@protonmail.com>
2022-04-23 17:57:57 -0500

    Add ci and deploy tasks to build.clj

--- build.clj
+++ build.clj
@@ -3,11 +3,27 @@
   (:require [clojure.tools.build.api :as b]
             [org.corfield.build :as bb]))
 
-(def lib 'com.github.john-shaffer/salmon)
+(def lib 'org.clojars.john-shaffer/salmon)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
+(defn get-version [opts]
+  (str version (when (:snapshot opts) "-SNAPSHOT")))
 
 (defn test [opts]
   (-> opts
       (assoc :main-args ["-m" "kaocha.runner"])
       (bb/run-task [:test]))
   opts)
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/clean
+      test
+      bb/clean
+      (assoc :src-pom "template/pom.xml")
+      bb/jar))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/deploy))
