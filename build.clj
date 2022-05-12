0dc43484bcfcc3130f02094bc1d034f7cf5610e5
John Shaffer <jshaffer2112@protonmail.com>
2022-05-12 17:39:17 -0500

    Version 0.6.0-SNAPSHOT

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'rs.shaffe/salmon)
-(def version "0.5.0")
+(def version "0.6.0")
 (defn get-version [opts]
   (str version (when (:snapshot opts) "-SNAPSHOT")))
 
