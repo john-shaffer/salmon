27d7992d720ed1bf5605660a96c9982ec07107bf
John Shaffer <jdsha@proton.me>
2022-09-12 10:49:30 -0500

    Use niv for nix commit pinning

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
 let
-  rev = "af0a9bc0e5341855518e9c1734d7ef913e5138b9";
-  nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "0qqxa8xpy1k80v5al45bsxqfs3n6cphm9nki09q7ara7yf7yyrh1";
-  };
-  pkgs = import nixpkgs {};
   jdk = pkgs.openjdk17;
 in with pkgs;
 mkShell {
