7efa0237f0f1604f89e677d71fc232a19f185a0e
John Shaffer <jdsha@proton.me>
2023-02-07 22:44:18 -0600

    Fix a bug in early-validate-conf with newer versions of donut.system
    The config was changed to be a distinct entry on the signal instead
    of being merged into the top-level of the signal.

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## Unreleased
+
+- Fix a bug in salmon.signal/early-validate-conf that caused it to fail
+  early-schema validation with newer versions of donut.system.
--- src/salmon/signal.clj
+++ src/salmon/signal.clj
@@ -7,9 +7,9 @@
  "Handles :salmon/early-validate signal by validating `conf` against the schema
   in the `:salmon/early-schema` entry of the component definition. Does nothing if
   there is no such entry."
-  [{:keys [->validation] ::ds/keys [system] :as signal}]
+  [{:keys [->validation] ::ds/keys [config system] :as signal}]
   (let [schema (-> system ::ds/component-def :salmon/early-schema)]
-    (when-let [errors (and schema (m/explain schema signal))]
+    (when-let [errors (and schema (m/explain schema config))]
       (->validation errors))))
 
 (defn- first-line [s]
