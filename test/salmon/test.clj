540313dd3c2a6cc736f049c245f22e3ec30f7749
John Shaffer <jdsha@pm.me>
2023-11-17 21:18:58 -0600

    Use a much more robust bucket name schema

--- test/salmon/test.clj
+++ test/salmon/test.clj
@@ -1,5 +1,6 @@
 (ns salmon.test
-  (:require [donut.system :as-alias ds]
+  (:require [clojure.string :as str]
+            [donut.system :as-alias ds]
             [malli.generator :as mg]
             [salmon.cloudformation :as cfn]
             [salmon.signal :as sig]))
@@ -13,16 +14,33 @@
 (defn get-config []
   default-config)
 
-(def ^{:doc "A regular expression for allowed S3 bucket names"}
+(def ^{:doc "A regular expression for matching S3 bucket names"}
   re-bucket-name
-  #"^[a-z0-9][a-z0-9-]{2,62}$")
+  #"^[a-z0-9][a-z0-9\-.]{1,61}[a-z0-9]$")
 
 (def ^{:doc "A regular expression for matching DNS labels"}
   re-dns-label
   #"^[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?$")
 
+(def ^{:doc "A regular expression for matching IPv4 addresses"}
+  re-ipv4-address
+  #"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+
+; https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+(def ^{:doc "A malli schema for S3 bucket names"}
+  bucket-name-schema
+  [:and
+   [:re re-bucket-name]
+   [:fn #(and
+           (not (str/includes? % ".."))
+           (not (str/ends-with? % "--ol-s3"))
+           (not (str/ends-with? % "-s3alias"))
+           (not (str/starts-with? % "sthree-"))
+           (not (str/starts-with? % "xn--"))
+           (not (re-matches re-ipv4-address %)))]])
+
 (defn rand-bucket-name []
-  (mg/generate [:re re-bucket-name]))
+  (mg/generate bucket-name-schema))
 
 (defn rand-dns-label []
   (mg/generate [:re re-dns-label]))
